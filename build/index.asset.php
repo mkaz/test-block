905c88b60ee6648d3a326bb2c51a82e66d0293df
Marcus Kazmierczak <marcus@mkaz.com>
2021-12-01 10:54:59 -0800

    Add front-end viewscript

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9a979822181b714842f15d7c08577e7d');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '46eeac3cf0517d56d24253bd831b4e6e');
